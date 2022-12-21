7107fb824d43d5fb3fc4a3823a9d001e0a9d5d58
Sanjay Tamboli <sanjay.tamboli@gmail.com>
2022-12-21 11:01:25 +0530

    -

---
+++ C Programming/Classwork/Print-Own-Detail.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("Fist Name: Sanjay\n");
+	printf("Last Name: Tamboli\n");
+	printf("Address: Motera, Sabarmati, Ahmedabad\n");
+	printf("Birth Date: 11-Dec-1964\n");
+}
