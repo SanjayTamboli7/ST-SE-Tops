d6bc320b35e62b384dce974ce59eeeb2205aef83
Sanjay Tamboli <sanjay.tamboli@gmail.com>
2023-01-13 10:11:38 +0530

    Add ArrayDemo1.c
    Add ArrayDemo1.c

---
+++ C Programming/Classwork/ArrayDemo1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+	int arr1[5] = {10, 15, 20, 25, 30};
+	printf("%d\t", arr1[0]);
+	printf("%d\t", arr1[1]);
+	printf("%d\t", arr1[2]);
+	printf("%d\t", arr1[3]);
+	printf("%d\t", arr1[4]);
+}
