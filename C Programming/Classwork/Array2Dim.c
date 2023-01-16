2918e3f3cf95cc4b41df96694d12d9c17507755a
Sanjay Tamboli <sanjay.tamboli@gmail.com>
2023-01-16 10:59:12 +0530

    2Dim array
    2Dim array

---
+++ C Programming/Classwork/Array2Dim.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+	{
+		int arr1[3][2] = {{10, 20}, {30, 40}, {50, 60}};
+		
+		printf("\n\t %d", arr1[0][0]);
+		printf("\t   %d", arr1[0][1]);
+
+		printf("\n\t %d", arr1[1][0]);
+		printf("\t   %d", arr1[1][1]);
+
+		printf("\n\t %d", arr1[2][0]);
+		printf("\t   %d", arr1[2][1]);
+	}
