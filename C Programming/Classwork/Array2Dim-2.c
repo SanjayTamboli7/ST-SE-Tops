fcb5f12da6b14a1fdefdc12b7b046c32ac9f311a
Sanjay Tamboli <sanjay.tamboli@gmail.com>
2023-01-20 09:39:29 +0530

    All C progs done in class
    All C progs done in class

--- C Programming/Classwork/Array2Dim-2.c
+++ C Programming/Classwork/Array2Dim-2.c
@@ -2,14 +2,22 @@
 
 void main()
 	{
-		int arr1[3][2] = {{10, 20}, {30, 40}, {50, 60}};
+		int i, j, arr1[3][2] = {{10, 20}, {30, 40}, {50, 60}};
 		
-		printf("\n\t %d", arr1[0][0]);
-		printf("\t   %d", arr1[0][1]);
-
-		printf("\n\t %d", arr1[1][0]);
-		printf("\t   %d", arr1[1][1]);
-
-		printf("\n\t %d", arr1[2][0]);
-		printf("\t   %d", arr1[2][1]);
+//		printf("\n\t %d", arr1[0][0]);
+//		printf("\t   %d", arr1[0][1]);
+//
+//		printf("\n\t %d", arr1[1][0]);
+//		printf("\t   %d", arr1[1][1]);
+//
+//		printf("\n\t %d", arr1[2][0]);
+//		printf("\t   %d", arr1[2][1]);
+		for(i=0;i<3;i++)
+			{
+				printf("\n");
+				for(j=0;j<2;j++)
+					{
+						printf("\%d\t", arr1[i][j]);
+					}
+			}
 	}
---
+++ C Programming/Classwork/nestedloop.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/*
+	for(initialization;condition;updation)
+	{
+		for(initialization;condition;updation)
+		{
+		
+		}	
+	}
+
+*/
+
+
+void main()
+{
+	int i,j;
+	
+	for(i=2;i<=7;i++)
+	{
+		for(j=1;j<=10;j++)
+		{
+			printf("%d X %d = %d\n",i,j,(i*j));
+		}
+			
+	}	
+	
+}
+
+
+
+
+
+
+
