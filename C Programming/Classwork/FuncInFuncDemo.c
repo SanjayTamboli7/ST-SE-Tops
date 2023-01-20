f2a32fe55f3bbf5049fae9f3a2b9343a9d18a64a
Sanjay Tamboli <sanjay.tamboli@gmail.com>
2023-01-20 10:44:32 +0530

    Create FuncInFuncDemo.c

---
+++ C Programming/Classwork/FuncInFuncDemo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void f1()
+	{
+		f2();
+		printf("\n\t Function 1 called.");
+	}
+
+void f2()
+	{
+		printf("\n\t Function 2 called.")
+	}
+
+void main()
+	{
+		printf("\n\t Before calling Function 1");
+		f1();
+		printf("\n\t Before calling Function 2");
+		f2();
+		printf("\n\t After calling Function 2");
+	}
