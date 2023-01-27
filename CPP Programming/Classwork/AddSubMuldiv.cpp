03052600c640777d4c1f1f7b7c63e2c031e32385
Sanjay Tamboli <sanjay.tamboli@gmail.com>
2023-01-27 10:20:02 +0530

    Class work 27-Jan-2023-1
    Class work 27-Jan-2023-1

---
+++ CPP Programming/Classwork/AddSubMuldiv.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+	{
+		int a = 20, b = 10;
+		cout << endl << "a              = " << a ;
+		cout << endl << "b              = " << b ;
+		cout << endl << "Addition       = " << (a+b);
+		cout << endl << "Subtraction    = " << (a-b);
+		cout << endl << "Multiplication = " << (a*b);
+		cout << endl << "Division       = " << (float)(a/b);
+		return 0;
+	}
---
+++ CPP Programming/Classwork/Welcome.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+	{
+		cout << endl << "Welcome to C++ programming.";
+		cout << endl << "In Top Technologies.";
+		return 0;
+	}
