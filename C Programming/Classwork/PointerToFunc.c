f73abccdb42a5efa06e8697db1ecf1a0a0e12df7
Sanjay Tamboli <sanjay.tamboli@gmail.com>
2023-02-11 15:18:42 +0530

    Class work for 6-Feb-2023
    Class work for 6-Feb-2023

---
+++ C Programming/Classwork/PointerToFunc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void swap(int *x, int *y)
+	{
+		int tmp;
+		tmp = *x;
+		*x = *y;
+		*y = tmp;
+	}
+
+void main()
+	{
+		int a = 5, b = 15;
+		printf("\n\t Value of A = %d, B = %d", a, b);
+		swap(&a, &b);
+		printf("\n\t Value of A = %d, B = %d", a, b);
+	}
---
+++ CPP Programming/Classwork/multipleinheritance-2.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+	public:
+		string name;
+		int rollno;
+	void getName(string n, int rn)
+	{
+		name = n;
+		rollno = rn;
+	}
+		
+	Student()
+	{
+		rollno = 41;
+	}		
+};
+class ExtraMarks
+{
+	public:
+		int sp;
+		
+	ExtraMarks()
+	{
+		sp = 55;
+	}
+};
+class Result : public Student,public ExtraMarks
+{
+	public:
+		int s1,s2,s3,tot;
+		float per;
+	Result(int phy, int chem, int bio)
+		{
+			s1 = phy;
+			s2 = chem;
+			s3 = bio;
+			tot = s1+s2+s3+sp;
+			per = (float)tot/4;
+		}
+		void Display()
+		{
+			cout<<"\nRoll No : "<<rollno;
+			cout<<"\nName : "<<name;
+			cout<<"\nTotal : "<<tot;
+			cout<<"\nPercentage : "<<per;
+		}		
+};
+
+int main()
+{
+	string name;
+	int RollNu, phy, chem, bio, spc;
+	cout<<"\nEnter your Name : ";
+	getline(cin,name);
+	cout << "Enter Roll Number : " ;
+	cin >> RollNu;
+	cout << "Enter Physics Marks : " ;
+	cin >> phy;
+	cout << "Enter Chemistry Marks : " ;
+	cin >> chem;
+	cout << "Enter Biology Marks : " ;
+	cin >> bio;
+//	cout << "Enter Special Marks : " ;
+//	cin >> spc;
+	Result res(phy, chem, bio);
+	res.getName(name, RollNu);	
+	res.Display();
+}
+
+
+
+
+
