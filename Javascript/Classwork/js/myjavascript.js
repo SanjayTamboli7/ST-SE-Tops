3460198564b96f1f5c58cd1b50e2f21f224c13e0
Sanjay Tamboli <sanjay.tamboli@gmail.com>
2023-08-26 08:34:16 +0530

    Commit 2023-Aug-25
    Commit 2023-Aug-25

---
+++ Javascript/Classwork/js/myjavascript.js
@@ -0,0 +1,25 @@
+function myFunction() 
+{
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function displayvalue()
+{
+    document.getElementById("demo").innerHTML = 5 + 6;
+}
+function windowalert()
+{
+    window.alert(5 + 7);
+}
+function consolelog()
+{
+    console.log(5+9);
+}
+function multiply(a, b) 
+{
+    document.getElementById("demo").innerHTML = "Result = " + (a * b);
+}
+function simplealert()
+{
+    alert(5+8);
+}
+
