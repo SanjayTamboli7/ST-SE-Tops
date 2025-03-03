2e23a932373f8708c1d35953bdeefd17ffcb9870
SanjayTamboli7 <120357040+SanjayTamboli7@users.noreply.github.com>
2025-03-03 14:32:36 +0530

    2025-Mar-0201
    2025-Mar-02

---
+++ Database/hms-backup.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.1.44-community : Database - hms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hms` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `hms`;
+
+/*Table structure for table `doctor` */
+
+DROP TABLE IF EXISTS `doctor`;
+
+CREATE TABLE `doctor` (
+  `Id` int(11) NOT NULL DEFAULT '0',
+  `NAME` varchar(30) DEFAULT NULL,
+  `SPECIALTY` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `doctor` */
+
+insert  into `doctor`(`Id`,`NAME`,`SPECIALTY`) values (1,'Mahesh N Shah','General Surgeon'),(2,'Naresh O Bhatt','Orthopedic Surgeon'),(3,'Omkar P Thathavadkar','ENT Surgeon');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
