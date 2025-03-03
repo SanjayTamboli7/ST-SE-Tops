4ef9392ec569603955c6d19da414f10485a085d9
SanjayTamboli7 <120357040+SanjayTamboli7@users.noreply.github.com>
2025-03-03 14:32:55 +0530

    2025-Mar-0202
    2025-Mar-0202

---
+++ Database/java-backup.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.1.44-community : Database - java
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`java` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `java`;
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `sid` int(10) NOT NULL AUTO_INCREMENT,
+  `sname` varchar(25) NOT NULL,
+  `city` varchar(35) DEFAULT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `student` */
+
+insert  into `student`(`sid`,`sname`,`city`) values (2,'Tops','G-Nagar'),(3,'Tops','H-Nagar'),(4,'Tops','J-Nagar');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `userid` int(10) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(15) DEFAULT NULL,
+  `lastname` varchar(15) DEFAULT NULL,
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`userid`,`firstname`,`lastname`,`username`,`password`) values (1,'Sanjay','Tamboli','Sanjay','pass'),(2,'Sanjay','Tamboli','Sanjay','pass');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
